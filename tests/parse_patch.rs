aecc0736c861eeafe1aa7db86353a3f080aed43c
Martin Pool <mbp@sourcefrog.net>
2024-08-18 13:02:01 -0700

    test: binary filenames containing spaces

--- tests/parse_patch.rs
+++ tests/parse_patch.rs
@@ -287,6 +287,18 @@ fn test_parse_triple_plus_minus_hack() {
 }
 
 #[test]
+fn binary_diff_with_spaces_in_name() {
+    let sample = "Binary files an old binary and a new binary differ\n";
+
+    let patch = Patch::from_single(sample).unwrap();
+    assert_eq!(patch.old.path, "an old binary");
+    assert_eq!(patch.old.meta, None);
+    assert_eq!(patch.new.path, "a new binary");
+    assert_eq!(patch.new.meta, None);
+    assert_eq!(patch.hunks, []);
+}
+
+#[test]
 fn single_binary_diff() {
     let sample = "Binary files old.bin and new.bin differ\n";
 
